a23b675b38b52cbdecb61c9897a1953f4d81e853
David Ji <davidji99@gmail.com>
2018-08-16 11:30:45 -0700

    initial tests [add-support-for-netrc]

--- heroku/provider.go
+++ heroku/provider.go
@@ -96,7 +96,7 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	}
 
 	log.Println("[INFO] Initializing Heroku client")
-	return config.Client()
+	return &config, nil
 }
 
 func buildCompositeID(a, b string) string {
